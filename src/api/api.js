37a59d675767cd8eb845f07369e30ded37995cc9
nehuenricardi <melmanpucacara@gmail.com>
2025-10-09 16:36:29 -0300

    ya-esta

--- src/api/api.js
+++ src/api/api.js
@@ -2,12 +2,12 @@ import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // ⚙ URL del backend (usa la IP real de tu PC en la misma red que el celular)
-const API_URL = "https://proyecto-sefy-backend.onrender.com";  
-
+const API_URL = "https://proyecto-sefy-backend-8m76.onrender.com";
+/* const API_URL = "http://128.3.254.143:8000"; */
 
 const api = axios.create({
   baseURL: API_URL,
-  timeout: 10000,
+  timeout: 1000000,
 });
 
 // Inyectar token JWT automáticamente
